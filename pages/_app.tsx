df956bec138e3f4f69f4faae66e9c49c6fa856ff
Dmitriy Shatalov <deman4ik@gmail.com>
2022-04-21 20:29:03 +0300

    feat: ym

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -37,6 +37,32 @@ export default function MyApp(props: AppProps & { colorScheme: ColorScheme }) {
                 />
                 <link rel="shortcut icon" type="image/x-icon" href="/favicon.svg" />
                 <link rel="shortcut icon" type="image/x-icon" href="/favicon.png" />
+                {process.env.NEXT_DISABLE_ANALYTICS ? null : (
+                    <>
+                        <script
+                            type="text/javascript"
+                            dangerouslySetInnerHTML={{
+                                __html: `
+                                (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                                m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                                (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+                             
+                                ym(69520861, "init", {
+                                     clickmap:true,
+                                     trackLinks:true,
+                                     accurateTrackBounce:true,
+                                     webvisor:true
+                                });
+                          `
+                            }}
+                        />
+                        <noscript
+                            dangerouslySetInnerHTML={{
+                                __html: `<noscript><div><img src="https://mc.yandex.ru/watch/69520861" style="position:absolute; left:-9999px;" alt="" /></div></noscript>`
+                            }}
+                        />
+                    </>
+                )}
             </Head>
             <SessionProvider session={pageProps.session}>
                 {/* <GraphqlProvider> */}
